991c191d8809830186a485f0cec4ca32c2ec164e
Laura Fiig <Laura Fiig>
2016-12-07 21:56:31 -0600

    initial friend array set up

---
+++ app/data/friends.js
@@ -0,0 +1,94 @@
+// friends data 
+// Initially populated with five 'starter' friends
+
+
+var friendArray = [
+  {
+  "name":"Luke",
+  "photo":"luke.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     2,
+     5,
+     4,
+     4,
+     5
+   ]
+},
+{
+  "name":"Percy",
+  "photo":"#",
+  "scores":[
+     2,
+     5,
+     2,
+     2,
+     4,
+     3,
+     2,
+     4,
+     3,
+     1
+   ]
+},
+{
+  "name":"Pixie",
+  "photo":"pixie.png",
+  "scores":[
+     4,
+     3,
+     4,
+     5,
+     3,
+     5,
+     5,
+     1,
+     4,
+     5
+   ]
+},
+{
+  "name":"Jennifer",
+  "photo":"jennifer.jpg",
+  "scores":[
+     4,
+     1,
+     2,
+     1,
+     5,
+     4,
+     4,
+     3,
+     2,
+     4
+   ]
+},
+{
+  "name":"Patty",
+  "photo":"Patty.jpg",
+  "scores":[
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3
+   ]
+}
+];
+
+// export the array
+module.exports = tableArray;
+
+
+
+
+
