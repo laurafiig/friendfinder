25a456924be5810a3466af9f3f988033ffa12e11
Laura Fiig <Laura Fiig>
2016-12-07 21:06:07 -0600

    server is set up, package.json and gitignore created

---
+++ server.js
@@ -0,0 +1,25 @@
+// npm packages
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// express server
+
+var app = express();
+// initial port
+var PORT = 8080;
+
+// BodyParser for our server to interpret data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//points our server to route files
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+// starts our server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
